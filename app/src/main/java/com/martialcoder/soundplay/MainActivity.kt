67cebe377aa41dee38af544d6d70dd4eaf5d60b7
suraj <surajkumarsahani1997@gmail.com>
2021-07-16 16:48:59 +0530

    random colors

--- app/src/main/java/com/martialcoder/soundplay/MainActivity.kt
+++ app/src/main/java/com/martialcoder/soundplay/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.martialcoder.soundplay
 
+import android.graphics.Color
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,36 +20,107 @@ class MainActivity : AppCompatActivity() {
 
         buttonOne.setOnClickListener {
 
+            // generate a new random number
+            val random = Random()
+
+            // generate a background color using RBG
+            val color =
+                Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256))
+
+            // set layout's background color
+            buttonOne.setBackgroundColor(color)
+
             mediaPlayer = MediaPlayer.create(this, R.raw.first)
             mediaPlayer?.start()
         }
         buttonTwo.setOnClickListener {
+
+            // generate a new random number
+            val random = Random()
+
+            // generate a background color using RBG
+            val color =
+                Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256))
+
+            // set layout's background color
+            buttonTwo.setBackgroundColor(color)
 
             mediaPlayer = MediaPlayer.create(this, R.raw.sound_file_3)
             mediaPlayer?.start()
         }
         buttonThree.setOnClickListener {
 
+            // generate a new random number
+            val random = Random()
+
+            // generate a background color using RBG
+            val color =
+                Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256))
+
+            // set layout's background color
+            buttonThree.setBackgroundColor(color)
+
             mediaPlayer = MediaPlayer.create(this, R.raw.sound_file_4)
             mediaPlayer?.start()
         }
         buttonFour.setOnClickListener {
+
+            // generate a new random number
+            val random = Random()
+
+            // generate a background color using RBG
+            val color =
+                Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256))
+
+            // set layout's background color
+            buttonFour.setBackgroundColor(color)
 
             mediaPlayer = MediaPlayer.create(this, R.raw.third)
             mediaPlayer?.start()
         }
         buttonFive.setOnClickListener {
 
+            // generate a new random number
+            val random = Random()
+
+            // generate a background color using RBG
+            val color =
+                Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256))
+
+            // set layout's background color
+            buttonFive.setBackgroundColor(color)
+
             mediaPlayer = MediaPlayer.create(this, R.raw.sound_file_5)
             mediaPlayer?.start()
         }
         buttonSix.setOnClickListener {
+
+            // generate a new random number
+            val random = Random()
+
+            // generate a background color using RBG
+            val color =
+                Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256))
+
+            // set layout's background color
+            buttonSix.setBackgroundColor(color)
 
 
             mediaPlayer = MediaPlayer.create(this, R.raw.sound_file_6)
             mediaPlayer?.start()
         }
         buttonSeven.setOnClickListener {
+
+            // generate a new random number
+            val random = Random()
+
+            // generate a background color using RBG
+            val color =
+                Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256))
+
+            // set layout's background color
+            buttonSeven.setBackgroundColor(color)
+
 
             mediaPlayer = MediaPlayer.create(this, R.raw.seven)
             mediaPlayer?.start()
