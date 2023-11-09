772068c5c3c984dff8421558096dae9da24994eb
SURAJ SAHANI <surajsahani@SURAJs-MacBook-Air.local>
2023-11-09 20:13:36 +0530

    resolving errors

--- app/src/main/java/com/martialcoder/soundplay/InAppUpdate.kt
+++ app/src/main/java/com/martialcoder/soundplay/InAppUpdate.kt
@@ -3,9 +3,8 @@ package com.martialcoder.soundplay
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.IntentSenderRequest
-import com.google.android.material.snackbar.Snackbar
+import androidx.activity.result.contract.ActivityResultContracts
+
 import com.google.android.play.core.appupdate.AppUpdateManager
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory
 import com.google.android.play.core.appupdate.AppUpdateOptions
@@ -13,12 +12,11 @@ import com.google.android.play.core.install.InstallStateUpdatedListener
 import com.google.android.play.core.install.model.AppUpdateType
 import com.google.android.play.core.install.model.InstallStatus
 import com.google.android.play.core.install.model.UpdateAvailability
-import androidx.activity.result.contract.ActivityResultContracts
-import com.google.android.play.core.appupdate.AppUpdateInfo
+import com.google.android.material.snackbar.Snackbar
 
 
 class InAppUpdate : AppCompatActivity() {
-    private lateinit var appUpdateManager:AppUpdateManager
+    private lateinit var appUpdateManager: AppUpdateManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,14 +25,23 @@ class InAppUpdate : AppCompatActivity() {
         appUpdateManager = AppUpdateManagerFactory.create(this)
         checkUpdates()
     }
-    private fun checkUpdates(){
+
+    private fun checkUpdates() {
+
+        // Returns an intent object that you use to check for an update.
         val appUpdateInfoTask = appUpdateManager.appUpdateInfo
-        appUpdateInfoTask.addOnSuccessListener { appApdateInfo ->
+
+        // Checks that the platform will allow the specified type of update.
+        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
             //update availability condition below code //means if update available
-            if(appApdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
-                &&  appApdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE)){
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                // This example applies an immediate update. To apply a flexible update
+                // instead, pass in AppUpdateType.FLEXIBLE
+                && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)
+            ) {
+                // Request the update.
                 appUpdateManager.startUpdateFlowForResult(
-                    appApdateInfo,
+                    appUpdateInfo,
                     activityResultLauncher, //passing activity result launcher
                     AppUpdateOptions.newBuilder(AppUpdateType.FLEXIBLE)
 //                        .setAllowAssetPackDeletion(true)   //if you want to delete old version from all aceess use true other vise you dont need to clear old version then remove this line
@@ -45,26 +52,28 @@ class InAppUpdate : AppCompatActivity() {
         appUpdateManager.registerListener(listener)
     }
 
-    private val listener= InstallStateUpdatedListener{state ->
+    private val listener = InstallStateUpdatedListener { state ->
         //check the condition
-        if (state.installStatus() == InstallStatus.DOWNLOADED){
+        if (state.installStatus() == InstallStatus.DOWNLOADED) {
             popupSnakebarForCompleteUpdate()
         }
     }
 
-    private  val activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()
-    ){ result :androidx.activity.result.ActivityResult ->
+    private val activityResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartIntentSenderForResult()
+    ) { result: androidx.activity.result.ActivityResult ->
         if (result.resultCode != RESULT_OK) {
             Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
         }
     }
-    fun popupSnakebarForCompleteUpdate(){
+
+    fun popupSnakebarForCompleteUpdate() {
         Snackbar.make(
             findViewById(R.id.button),
             "An update has just been downloaded.",
             Snackbar.LENGTH_INDEFINITE
         ).apply {
-            setAction("Install"){appUpdateManager.completeUpdate() }
+            setAction("Install") { appUpdateManager.completeUpdate() }
                 .setActionTextColor(getColor(android.R.color.holo_blue_dark))
             show()
         }
@@ -77,8 +86,8 @@ class InAppUpdate : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        appUpdateManager.appUpdateInfo.addOnSuccessListener {appUpdateInfo ->
-            if (appUpdateInfo.installStatus() == InstallStatus.DOWNLOADED){
+        appUpdateManager.appUpdateInfo.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.installStatus() == InstallStatus.DOWNLOADED) {
                 popupSnakebarForCompleteUpdate()
             }
         }
